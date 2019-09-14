8a782ca1349372694317a1b581144f523fb3a2a3
Aditya Rathod <15635979+applecrazy@users.noreply.github.com>
2019-09-13 21:10:01 -0500

    Add redesigned homepage and supporting code

---
+++ index.js
@@ -0,0 +1,15 @@
+var typed = new Typed('.loves-animated', {
+    strings: ["", "data science.", "machine learning.", "building amazing things.", "solving challenging problems."],
+    typeSpeed: 40,
+    backSpeed: 70,
+    backDelay: 3500,
+    startDelay: 300,
+    cursorChar: '|',
+    onComplete: () => {
+        setTimeout(() => {
+            var x = document.querySelector('.typed-cursor')
+            x.parentNode.removeChild(x);
+        }, 1000)
+    }
+    // loop: true
+});
