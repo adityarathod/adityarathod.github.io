a5a17ad60145705fb31163486d81d4a1a6169510
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2020-07-11 13:41:03 -0700

    Split navbar link/divider and navbar itself into components

---
+++ components/shared/TopLink.tsx
@@ -0,0 +1,42 @@
+import React, { FC, useContext } from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import classNames from 'classnames'
+import { ThemeContext } from '../../pages/_app'
+
+interface TopLinkProps {
+	href: string
+	text: string
+}
+
+const TopLink: FC<TopLinkProps> = (props: TopLinkProps) => {
+	const { theme } = useContext(ThemeContext)
+	const router = useRouter()
+	const darkMode = theme === 'dark'
+	const isCurPath = router.pathname === props.href
+	return (
+		<Link href={isCurPath ? '#' : props.href}>
+			<a
+				className={classNames(
+					!isCurPath ? 'cursor-pointer' : 'cursor-default',
+					'px-4',
+					!isCurPath
+						? darkMode
+							? 'hover:text-link-dark'
+							: 'hover:text-link-light'
+						: null,
+					isCurPath
+						? darkMode
+							? 'text-secondaryLabel-dark'
+							: 'text-secondaryLabel-light'
+						: darkMode
+						? 'text-label-dark'
+						: 'text-label-light'
+				)}>
+				{props.text}
+			</a>
+		</Link>
+	)
+}
+
+export default TopLink
---
+++ components/shared/TopNav.tsx
@@ -0,0 +1,61 @@
+import React, { FC, useContext } from 'react'
+import classNames from 'classnames'
+import { ThemeContext } from '../../pages/_app'
+import TopLink from './TopLink'
+import TopLinkDivider from './TopLinkDivider'
+
+const TopNav: FC = () => {
+	const { theme, toggleTheme } = useContext(ThemeContext)
+	const darkMode = theme === 'dark'
+	return (
+		<div className='mb-8'>
+			{/* dark mode toggle */}
+			<button
+				className={classNames(
+					'absolute',
+					'text-sm',
+					'font-bold',
+					'outline-none',
+					darkMode ? 'text-systemBlue-dark' : 'text-systemBlue-light'
+				)}
+				style={{ top: '10px', right: '10px', outline: 'none' }}
+				onClick={() => toggleTheme()}>
+				{theme} mode
+			</button>
+			<button
+				className={classNames(
+					'absolute',
+					'text-sm',
+					'font-bold',
+					'outline-none',
+					darkMode ? 'text-systemBlue-dark' : 'text-systemBlue-light'
+				)}
+				style={{ top: '10px', right: '10px', outline: 'none' }}
+				onClick={() => toggleTheme()}>
+				{theme} mode
+			</button>
+			<div className='mb-8'>
+				<h1
+					className={classNames(
+						'text-3xl',
+						'text-center',
+						'font-bold',
+						darkMode ? 'text-label-dark' : 'text-label-light'
+					)}>
+					Aditya Rathod
+				</h1>
+				<div className='px-4 max-w-md mx-auto my-0 flex flex-row items-center justify-between'>
+					<TopLink href='/' text='home' />
+					<TopLinkDivider />
+					<TopLink href='//github.com/adityarathod' text='github' />
+					<TopLinkDivider />
+					<TopLink href='/resume.pdf' text='resumé' />
+					<TopLinkDivider />
+					<TopLink href='/blog' text='blog' />
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default TopNav
