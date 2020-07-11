aac71a3643d68885bace3033378948a76c0bca32
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2020-07-10 22:17:48 -0700

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,149 @@
+module.exports = {
+  purge: [],
+  theme: {
+    colors: {
+      label: {
+        light: 'rgba(0.0, 0.0, 0.0, 1.0)',
+        dark: 'rgba(255.0, 255.0, 255.0, 1.0)'
+      },
+      secondaryLabel: {
+        light: 'rgba(60.0, 60.0, 67.0, 0.6)',
+        dark: 'rgba(235.0, 235.0, 245.0, 0.6)'
+      },
+      tertiaryLabel: {
+        light: 'rgba(60.0, 60.0, 67.0, 0.3)',
+        dark: 'rgba(235.0, 235.0, 245.0, 0.3)'
+      },
+      quaternaryLabel: {
+        light: 'rgba(60.0, 60.0, 67.0, 0.18)',
+        dark: 'rgba(235.0, 235.0, 245.0, 0.18)'
+      },
+      systemFill: {
+        light: 'rgba(120.0, 120.0, 128.0, 0.2)',
+        dark: 'rgba(120.0, 120.0, 128.0, 0.36)'
+      },
+      secondarySystemFill: {
+        light: 'rgba(120.0, 120.0, 128.0, 0.16)',
+        dark: 'rgba(120.0, 120.0, 128.0, 0.32)'
+      },
+      tertiarySystemFill: {
+        light: 'rgba(118.0, 118.0, 128.0, 0.12)',
+        dark: 'rgba(118.0, 118.0, 128.0, 0.24)'
+      },
+      quaternarySystemFill: {
+        light: 'rgba(116.0, 116.0, 128.0, 0.08)',
+        dark: 'rgba(118.0, 118.0, 128.0, 0.18)'
+      },
+      placeholderText: {
+        light: 'rgba(60.0, 60.0, 67.0, 0.3)',
+        dark: 'rgba(235.0, 235.0, 245.0, 0.3)'
+      },
+      systemBackground: {
+        light: 'rgba(255.0, 255.0, 255.0, 1.0)',
+        dark: 'rgba(0.0, 0.0, 0.0, 1.0)'
+      },
+      secondarySystemBackground: {
+        light: 'rgba(242.0, 242.0, 247.0, 1.0)',
+        dark: 'rgba(28.0, 28.0, 30.0, 1.0)'
+      },
+      tertiarySystemBackground: {
+        light: 'rgba(255.0, 255.0, 255.0, 1.0)',
+        dark: 'rgba(44.0, 44.0, 46.0, 1.0)'
+      },
+      systemGroupedBackground: {
+        light: 'rgba(242.0, 242.0, 247.0, 1.0)',
+        dark: 'rgba(0.0, 0.0, 0.0, 1.0)'
+      },
+      secondarySystemGroupedBackground: {
+        light: 'rgba(255.0, 255.0, 255.0, 1.0)',
+        dark: 'rgba(28.0, 28.0, 30.0, 1.0)'
+      },
+      tertiarySystemGroupedBackground: {
+        light: 'rgba(242.0, 242.0, 247.0, 1.0)',
+        dark: 'rgba(44.0, 44.0, 46.0, 1.0)'
+      },
+      separator: {
+        light: 'rgba(60.0, 60.0, 67.0, 0.29)',
+        dark: 'rgba(84.0, 84.0, 88.0, 0.6)'
+      },
+      opaqueSeparator: {
+        light: 'rgba(198.0, 198.0, 200.0, 1.0)',
+        dark: 'rgba(56.0, 56.0, 58.0, 1.0)'
+      },
+      link: {
+        light: 'rgba(0.0, 122.0, 255.0, 1.0)',
+        dark: 'rgba(9.0, 132.0, 255.0, 1.0)'
+      },
+      darkText: {
+        light: 'rgba(0.0, 0.0, 0.0, 1.0)',
+        dark: 'rgba(0.0, 0.0, 0.0, 1.0)'
+      },
+      lightText: {
+        light: 'rgba(255.0, 255.0, 255.0, 0.6)',
+        dark: 'rgba(255.0, 255.0, 255.0, 0.6)'
+      },
+      systemBlue: {
+        light: 'rgba(0.0, 122.0, 255.0, 1.0)',
+        dark: 'rgba(10.0, 132.0, 255.0, 1.0)'
+      },
+      systemGreen: {
+        light: 'rgba(52.0, 199.0, 89.0, 1.0)',
+        dark: 'rgba(48.0, 209.0, 88.0, 1.0)'
+      },
+      systemIndigo: {
+        light: 'rgba(88.0, 86.0, 214.0, 1.0)',
+        dark: 'rgba(94.0, 92.0, 230.0, 1.0)'
+      },
+      systemOrange: {
+        light: 'rgba(255.0, 149.0, 0.0, 1.0)',
+        dark: 'rgba(255.0, 159.0, 10.0, 1.0)'
+      },
+      systemPink: {
+        light: 'rgba(255.0, 45.0, 85.0, 1.0)',
+        dark: 'rgba(255.0, 55.0, 95.0, 1.0)'
+      },
+      systemPurple: {
+        light: 'rgba(175.0, 82.0, 222.0, 1.0)',
+        dark: 'rgba(191.0, 90.0, 242.0, 1.0)'
+      },
+      systemRed: {
+        light: 'rgba(255.0, 59.0, 48.0, 1.0)',
+        dark: 'rgba(255.0, 69.0, 58.0, 1.0)'
+      },
+      systemTeal: {
+        light: 'rgba(90.0, 200.0, 250.0, 1.0)',
+        dark: 'rgba(100.0, 210.0, 255.0, 1.0)'
+      },
+      systemYellow: {
+        light: 'rgba(255.0, 204.0, 0.0, 1.0)',
+        dark: 'rgba(255.0, 214.0, 10.0, 1.0)'
+      },
+      systemGray: {
+        light: 'rgba(142.0, 142.0, 147.0, 1.0)',
+        dark: 'rgba(142.0, 142.0, 147.0, 1.0)'
+      },
+      systemGray2: {
+        light: 'rgba(174.0, 174.0, 178.0, 1.0)',
+        dark: 'rgba(99.0, 99.0, 102.0, 1.0)'
+      },
+      systemGray3: {
+        light: 'rgba(199.0, 199.0, 204.0, 1.0)',
+        dark: 'rgba(72.0, 72.0, 74.0, 1.0)'
+      },
+      systemGray4: {
+        light: 'rgba(209.0, 209.0, 214.0, 1.0)',
+        dark: 'rgba(58.0, 58.0, 60.0, 1.0)'
+      },
+      systemGray5: {
+        light: 'rgba(229.0, 229.0, 234.0, 1.0)',
+        dark: 'rgba(44.0, 44.0, 46.0, 1.0)'
+      },
+      systemGray6: {
+        light: 'rgba(242.0, 242.0, 247.0, 1.0)',
+        dark: 'rgba(28.0, 28.0, 30.0, 1.0)'
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+}
