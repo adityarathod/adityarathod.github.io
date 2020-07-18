8e8d12d6838c2c58998064b3695f35ce9dccb0fd
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2020-07-18 14:59:07 -0700

    Add KaTeX and math support

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const rehypePrism = require('@mapbox/rehype-prism')
+const remarkMath = require('remark-math')
+const rehypeKatex = require('rehype-katex')
+
 const withMDX = require('@next/mdx')({
 	extension: /\.mdx?$/,
 	options: {
-		hastPlugins: [rehypePrism],
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypePrism, rehypeKatex],
 	},
 })
 module.exports = withMDX({
