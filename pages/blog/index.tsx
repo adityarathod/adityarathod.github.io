c616dfafe27b46723a4a87c9d57aeef9d79f4858
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2020-07-18 15:05:08 -0700

    Add posts, images, code block theme, and make blog posts clickable

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -3,6 +3,7 @@ import classNames from 'classnames'
 import posts from '~/data/posts'
 import Layout from '~/components/shared/Layout'
 import { ThemeContext } from '../_app'
+import Link from 'next/link'
 
 const Blog = () => {
 	const { theme } = useContext(ThemeContext)
@@ -23,40 +24,41 @@ const Blog = () => {
 			</h1>
 			<div className='my-0 mx-auto max-w-4xl px-6 cursor-pointer'>
 				{posts.map((post, idx) => (
-					<div
-						key={idx}
-						className={classNames(
-							'mt-12',
-							'py-6',
-							'px-6',
-							'rounded-lg',
-							theme === 'dark' ? 'text-label-dark' : 'text-label-light',
-							theme === 'dark'
-								? 'bg-secondarySystemBackground-dark'
-								: 'bg-secondarySystemBackground-light'
-						)}>
-						<h3 className={classNames('text-2xl', 'font-medium')}>{post.title}</h3>
-						<h4
+					<Link href={`/blog/${post.slug}`} key={idx}>
+						<div
 							className={classNames(
-								'text-lg',
-								// 'max-w-2xl',
+								'mt-12',
+								'py-6',
+								'px-6',
+								'rounded-lg',
+								theme === 'dark' ? 'text-label-dark' : 'text-label-light',
 								theme === 'dark'
-									? 'text-secondaryLabel-dark'
-									: 'text-secondaryLabel-light'
+									? 'bg-secondarySystemBackground-dark'
+									: 'bg-secondarySystemBackground-light'
 							)}>
-							{post.description}
-						</h4>
-						<h5
-							className={classNames(
-								'text-md',
-								// 'max-w-2xl',
-								theme === 'dark'
-									? 'text-tertiaryLabel-dark'
-									: 'text-tertiaryLabel-light'
-							)}>
-							{post.date}
-						</h5>
-					</div>
+							<h3 className={classNames('text-2xl', 'font-medium')}>{post.title}</h3>
+							<h4
+								className={classNames(
+									'text-lg',
+									// 'max-w-2xl',
+									theme === 'dark'
+										? 'text-secondaryLabel-dark'
+										: 'text-secondaryLabel-light'
+								)}>
+								{post.description}
+							</h4>
+							<h5
+								className={classNames(
+									'text-md',
+									// 'max-w-2xl',
+									theme === 'dark'
+										? 'text-tertiaryLabel-dark'
+										: 'text-tertiaryLabel-light'
+								)}>
+								{post.date}
+							</h5>
+						</div>
+					</Link>
 				))}
 			</div>
 		</Layout>
