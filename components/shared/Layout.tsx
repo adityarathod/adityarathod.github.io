265601deb3b14231cae2fec956198ee3b973792e
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2020-07-19 09:24:08 -0700

    Add GA for page view tracking

--- components/shared/Layout.tsx
+++ components/shared/Layout.tsx
@@ -1,17 +1,23 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 import TopNav from './TopNav'
 import Footer from './Footer'
+import { initialize, logPageView } from '~/utils/google-analytics'
 
 interface LayoutProps {
 	children: React.ReactNode
 }
 
-const Layout: FC<LayoutProps> = (props: LayoutProps) => (
-	<>
-		<TopNav />
-		{props.children}
-		<Footer />
-	</>
-)
-
+const Layout: FC<LayoutProps> = (props: LayoutProps) => {
+	useEffect(() => {
+		initialize()
+		logPageView()
+	}, [])
+	return (
+		<>
+			<TopNav />
+			{props.children}
+			<Footer />
+		</>
+	)
+}
 export default Layout
