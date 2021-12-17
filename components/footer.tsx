06d98bec182f98b33c835122a3baddfb41fab4f3
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2021-12-17 00:15:08 -0800

    Initial gatsby commit

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import React, { FC } from 'react'
 const Footer: FC = () => {
 	return (
 		<div className='mt-32 mb-8 opacity-80'>
-			<p className='mt-4 text-xs'>&copy; 2020-present Aditya Rathod. All rights reserved.</p>
+			<p className='mt-4 text-xs'>&copy; 2021-present Aditya Rathod. All rights reserved.</p>
 		</div>
 	)
 }
