f82b5d7856c0e89025afe3dff34e0279d6c7c898
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2021-12-19 18:19:48 -0800

    Add meta tags to homepage

--- gatsby/src/components/meta.tsx
+++ gatsby/src/components/meta.tsx
@@ -30,3 +30,26 @@ export const ArticleMeta: FC<ArticleMetaProps> = ({ title, description, domain, 
 		</Helmet>
 	)
 }
+
+const Meta: FC<ArticleMetaProps> = ({ title, description, domain, url, image }) => {
+	return (
+		<Helmet>
+			<meta name='description' content={description} />
+			{/* OpenGraph properties */}
+			<meta property='og:url' content={url} />
+			<meta property='og:type' content='website' />
+			<meta property='og:title' content={title} />
+			<meta property='og:description' content={description} />
+			<meta property='og:image' content={image} />
+			{/* Twitter properties */}
+			<meta name='twitter:card' content='summary_large_image' />
+			<meta property='twitter:domain' content={domain} />
+			<meta property='twitter:url' content={url} />
+			<meta name='twitter:title' content={title} />
+			<meta name='twitter:description' content={description} />
+			<meta name='twitter:image' content={image} />
+		</Helmet>
+	)
+}
+
+export default Meta
--- gatsby/src/pages/index.tsx
+++ gatsby/src/pages/index.tsx
@@ -1,13 +1,36 @@
 import React, { FC } from 'react'
-import { PageProps } from 'gatsby'
+import { graphql, PageProps, useStaticQuery } from 'gatsby'
 import Layout from '../components/layout'
+import Meta from '../components/meta'
 import Hero from '../components/hero'
 import Navbar from '../components/navbar'
 import Projects from '../components/projects'
 
 const IndexPage: FC<PageProps> = () => {
+	const {
+		data: {
+			site: {
+				siteMetadata: { siteUrl },
+			},
+		},
+	} = useStaticQuery(graphql`
+		query {
+			site {
+				siteMetadata {
+					siteUrl
+				}
+			}
+		}
+	`)
 	return (
 		<Layout title='Aditya Rathod | Home'>
+			<Meta
+				title='Aditya Rathod'
+				description='Aditya is a computer science junior at The University of Texas at Dallas.'
+				domain={siteUrl}
+				image={`${siteUrl}/og-image.png`}
+				url={siteUrl}
+			/>
 			<Navbar />
 			<Hero />
 			<section className='pt-24 max-w-3xl mx-auto'>
